d0ddca5579c17f893a84b062fc33a51c4d7df70b
Luis <luuish9@gmail.com>
2026-04-05 18:52:06 -0400

    feat: add usuario prisma schema and migration

---
+++ prisma/migrations/20260405020115_create_table_usuario/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "usuario" (
+    "id" SERIAL NOT NULL,
+    "nome_usuario" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+
+    CONSTRAINT "usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuario_email_key" ON "usuario"("email");
